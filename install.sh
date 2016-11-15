65acc40d175177e0b9685d9bb39e179ffc8a11a0
Simon Sinding <thesinding@a.lan>
2016-11-15 22:22:03 +0100

    Added to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
-echo "Installing pastey to /usr/local/bin/"
+printf "Making pastey executable"
+chmod +x pastey
+printf "... ${BOLD}Done\n"
+printf "Installing pastey to /usr/local/bin/"
 cp pastey /usr/local/bin
+printf "... ${BOLD}Done\n"
 echo "Pastey is now installed"
